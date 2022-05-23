d082adbb30f17e50e2aea00c0dcf44f01ad77f71
vinspdb <57073656+vinspdb@users.noreply.github.com>
2022-05-23 22:21:40 +0200

    Update utility.py

--- SeedGeneration/utility.py
+++ SeedGeneration/utility.py
@@ -1,16 +1,34 @@
 import numpy as np
 
-def get_sequence(prova, max_trace, mean_trace, n_act):
+def remove_consecutive_duplicates(trace):
+        temp = []
+        cont = 0
+        i = 0
+        while i<len(trace):
+            if i == 0:
+                temp.append(trace[i])
+            else:
+                if trace[i-1] == trace[i]:
+                    cont = cont + 1
+                    if cont < 2:
+                        temp.append(trace[i])
+                else:
+                    cont = 0
+                    temp.append(trace[i])
+            i = i + 1
+        return temp
+
+def get_sequence(sequence, max_trace, mean_trace, n_act):
     i = 0
     s = (max_trace)
     list_seq = []
     list_label = []
-    while i < len(prova):
+    while i < len(sequence):
         list_temp = []
         seq = np.zeros(s)
         j = 0
-        while j < (len(prova.iat[i, 0]) - 1):
-            list_temp.append(prova.iat[i, 0][0 + j])
+        while j < (len(sequence[i]) - 1):
+            list_temp.append(sequence[i][j])
             new_seq = np.append(seq, list_temp)
             cut = len(list_temp)
             new_seq = new_seq[cut:]
@@ -18,7 +36,7 @@ def get_sequence(prova, max_trace, mean_trace, n_act):
                 pass
             else:
                 list_seq.append(new_seq[-mean_trace:])
-                list_label.append(prova.iat[i, 0][j + 1])
+                list_label.append(sequence[i][j + 1])
             j = j + 1
         i = i + 1
     return list_seq, list_label
